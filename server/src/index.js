53c78ffbf82125a0cc0b805aa09a845b56603d9f
Mihai <mihai_visovan@yahoo.com>
2022-03-06 16:10:39 +0200

    Added static folder and prepare SPA

--- server/src/index.js
+++ server/src/index.js
@@ -3,6 +3,7 @@ import bodyParser from 'body-parser';
 import cors from 'cors';
 import 'dotenv/config';
 import posts from './routes/api/posts.js';
+import path from 'path';
 
 const app = express();
 
@@ -12,6 +13,15 @@ app.use(cors());
 
 // Routes
 app.use('/api/posts', posts);
+
+// Handle production
+if (process.env.NODE_ENV === 'production') {
+  // Static folder
+  app.use(express.static(path.resolve(__dirname, '/static/')));
+
+  // Handle SPA
+  app.get('.*'), (req, res) => res.sendFile(path.resolve(__dirname, '/static/index.html'));
+}
 
 // Server
 const port = process.env.PORT || 5000;
