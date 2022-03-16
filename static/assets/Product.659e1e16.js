8aaa2608ff87df72a19a98b265d9e14d1ead7f4b
Mihai <mihai_visovan@yahoo.com>
2022-03-16 23:13:59 +0200

    Added vuex, responsiveness and global styling

---
+++ static/assets/Product.659e1e16.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./index.913bce18.js";import{o as r,c as e}from"./vendor.01b4222a.js";const t={name:"Product"};function c(a,n,s,p,_,d){return r(),e("div")}var i=o(t,[["render",c]]);export{i as default};
