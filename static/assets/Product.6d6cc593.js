79f72e09e182f86b60b062dc2a20ecb0a38017cf
Mihai <mihai_visovan@yahoo.com>
2022-03-20 01:08:47 +0200

    FE build

---
+++ static/assets/Product.6d6cc593.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./index.e0cc8362.js";import{o as r,c as e}from"./vendor.78f0539e.js";const t={name:"Product"};function c(a,n,s,p,_,d){return r(),e("div")}var i=o(t,[["render",c]]);export{i as default};
